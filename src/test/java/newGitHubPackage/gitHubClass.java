0e5575be9e5c28ded967515afa041b5cd871080f
Mohamed <engmjamal92@gmail.com>
2024-11-07 16:41:46 +0200

    from master branch

---
+++ src/test/java/newGitHubPackage/gitHubClass.java
@@ -0,0 +1,11 @@
+package newGitHubPackage;
+
+import org.testng.annotations.Test;
+
+public class gitHubClass {
+
+    @Test
+    public void methodForGitHub(){
+        System.out.println("this is a method for github");
+    }
+}
