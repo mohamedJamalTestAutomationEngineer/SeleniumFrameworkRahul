d60373fee19705c7e3d230322f741ae9ee1c8a3b
Mohamed <engmjamal92@gmail.com>
2024-11-07 16:14:07 +0200

    from branch develop , created new package with new class

---
+++ src/test/java/PackageGitHub/branch1.java
@@ -0,0 +1,11 @@
+package PackageGitHub;
+
+import org.testng.annotations.Test;
+
+public class branch1 {
+
+    @Test
+    public void Branch1(){
+        System.out.println("this is branch 1");
+    }
+}
