d0344b7e3dd870664a9d780d1d489dc707b43e03
Mohamed <engmjamal92@gmail.com>
2024-11-07 14:51:49 +0200

    from second Project

---
+++ src/test/java/org/groupID/testArtifactID/thirdClass.java
@@ -0,0 +1,12 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+import testingComponents.BaseTest;
+
+public class thirdClass extends BaseTest {
+
+    @Test
+    public void methodfromthe2ndproject(){
+        System.out.println("this is a method from the 2nd project");
+    }
+}
