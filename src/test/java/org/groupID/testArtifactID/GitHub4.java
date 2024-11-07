7e093cb48a42f375c3f778cb6ce73bf1513e42d6
Mohamed <engmjamal92@gmail.com>
2024-11-07 15:59:04 +0200

    commit on the second branch develop

---
+++ src/test/java/org/groupID/testArtifactID/GitHub4.java
@@ -0,0 +1,11 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+
+public class GitHub4 {
+
+    @Test
+    public void GitHub4(){
+        System.out.println("this is GitHub 4");
+    }
+}
---
+++ src/test/java/org/groupID/testArtifactID/GitHub5.java
@@ -0,0 +1,11 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+
+public class GitHub5 {
+
+    @Test
+    public void GitHub4(){
+        System.out.println("this is GitHub 5");
+    }
+}
