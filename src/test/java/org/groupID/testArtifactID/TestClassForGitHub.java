3c5cbc8cef114485c2a8566d04d094dda31f0283
Mohamed <engmjamal92@gmail.com>
2024-11-07 14:06:37 +0200

    commit from intellJ code

---
+++ src/test/java/org/groupID/testArtifactID/TestClassForGitHub.java
@@ -0,0 +1,12 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+import testingComponents.BaseTest;
+
+public class TestClassForGitHub extends BaseTest {
+
+    @Test
+    public void testmethod(){
+        System.out.println("test method");
+    }
+}
